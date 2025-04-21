46338bd3cda19fc6a375c5386173b6237af0cca1
23026475 <ndivhuswiswi@gmail.com>
2025-04-21 17:18:27 +0200

    Add project files.

---
+++ MintysGo1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MintysGo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
